89f700703f7866a9ee5b451c40f72804b7b36e5e
Anne L'Hôte <anne.lhote@gmail.com>
2016-10-07 16:14:51 +0200

    Add jqueryui

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -43,10 +43,12 @@ $coralURL = $util->getCORALURL();
 <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="css/thickbox.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="css/datePicker.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/jquery-ui-1.9.2.min.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="css/jquery.autocomplete.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="css/jquery.tooltip.css" type="text/css" media="screen" />
 <link rel="SHORTCUT ICON" href="images/butterflyfishfavicon.ico" />
 <script type="text/javascript" src="js/plugins/jquery.js"></script>
+<script type="text/javascript" src="js/plugins/jquery-ui-1.9.2.min.js"></script>
 <script type="text/javascript" src="js/plugins/ajaxupload.3.5.js"></script>
 <script type="text/javascript" src="js/plugins/thickbox.js"></script>
 <script type="text/javascript" src="js/plugins/jquery.autocomplete.js"></script>
